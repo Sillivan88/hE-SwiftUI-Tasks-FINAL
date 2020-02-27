0cb196a74268d90cac275ed885aafaf757ed3b3f
Thomas Sillmann <thomas@thomassillmann.de>
2020-02-27 12:32:17 +0100

    Initial Commit

---
+++ TS SwiftUI Tasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TS SwiftUI Tasks
+//
+//  Created by Thomas Sillmann on 27.02.20.
+//  Copyright © 2020 Thomas Sillmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
